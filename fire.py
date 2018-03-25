5af75912d122a56e133afe886bc8289e52e7225b
jzheadley <headleyjz@vcu.edu>
2018-03-25 06:08:03 +0000

    java code builds a nice jar and hopefully will make the call out to the python code

--- fire.py
+++ fire.py
@@ -1,4 +1,23 @@
-from armageddon import Armageddon
+import roombaApi as roomba
+from missileApi import MissileApi 
+import threading
 
-instance = Armageddon()
-instance.send_cmd(instance.FIRE)
+
+def asyncTargetDetected(middle):
+    roomba.targetDetected(middle)
+
+launcher = MissileApi()
+fireThread = threading.Thread(target=launcher.fire,args=(),kwargs={})
+targetedThread = threading.Thread(target=asyncTargetDetected,args=(sys.argv[1]),kwargs={})
+
+roomba.initialize()
+targetedThread.start()
+fireThread.start()
+
+
+
+targetedThread.join()
+fireThread.join()
+
+
+
