8b1f87c0f0ca3df7cd3b270730d3dfd07c7c3b42
jzheadley <headleyjz@vcu.edu>
2018-03-25 07:39:27 +0000

    fixed up fire.py to read the middle coord from a file written by java side

--- fire.py
+++ fire.py
@@ -2,18 +2,23 @@ import roombaApi as roomba
 from missileApi import MissileApi 
 import threading
 import sys
+import time
 
 
 def asyncTargetDetected(middle):
     roomba.targetDetected(middle)
 
 launcher = MissileApi()
+file=open("middle.txt","r")
+middle = float(file.readline())
+targetedThread = threading.Thread(target=asyncTargetDetected,args=(),kwargs={'middle': middle})
+time.sleep(1)
 fireThread = threading.Thread(target=launcher.fire,args=(),kwargs={})
-targetedThread = threading.Thread(target=asyncTargetDetected,args=(),kwargs={'middle': sys.argv[1]})
 
 roomba.initialize()
 targetedThread.start()
 fireThread.start()
+
 
 
 
